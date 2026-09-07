7464b3d4d4ae2fdf80a34a4e0e893ba80ab8f7b6
Dinh Van Tai <dinhvantai079@gmail.com>
2026-09-07 19:59:57 +0700

    redo Court

--- Contract.Repositories/Entity/Court.cs
+++ Contract.Repositories/Entity/Court.cs
@@ -6,6 +6,8 @@
 
         public int CourtTypeId { get; set; }
 
+        public int? BranchId { get; set; }
+
         public string CourtCode { get; set; } = string.Empty;
 
         public string CourtName { get; set; } = string.Empty;
@@ -26,7 +28,8 @@
 
         public DateTime? UpdatedAt { get; set; }
 
-        public ICollection<BookingDetail> BookingDetails { get; set; }
-            = new List<BookingDetail>();
+        public Branch? Branch { get; set; }
+
+        public ICollection<BookingDetail> BookingDetails { get; set; } = new List<BookingDetail>();
     }
 }
